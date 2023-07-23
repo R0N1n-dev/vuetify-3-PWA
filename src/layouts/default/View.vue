7d69c118def342111fe9591e7e4482deb91169db
Kalungi Moses <mdrewj@live.com>
2023-07-23 15:21:08 +0300

    Initial commit: test PWA deployment

---
+++ src/layouts/default/View.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-toolbar dark prominent image="../../assets/vbanner.jpg">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ vite.config.js
@@ -0,0 +1,77 @@
+// Plugins
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import { VitePWA } from "vite-plugin-pwa";
+// Utilities
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      /*devOptions: {
+        enabled: true,
+      },*/
+      workbox: {
+        globPatterns: [
+          // all packaged resources are stored here
+          "assets/*",
+          // add HTML and other resources for the root directory
+          "*.{svg,png,jpg}",
+          "*.html",
+          //"manifest.webmanifest",
+          // be careful, not to add sw.js
+        ],
+      },
+      manifest: {
+        name: "Vuetify 3 PWA ",
+        short_name: "VuetiPWA",
+        description: "Vuetify 3 PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+  define: { "process.env": {} },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+  },
+  server: {
+    port: 3000,
+  },
+});
